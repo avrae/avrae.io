5c5f70acc50ae68fb056e9ee6144ff289c276f43
Andrew Zhu <mommothazaz123@gmail.com>
2018-08-30 22:14:33 -0700

    add db interfaces

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,21 @@
+import os
+
 from flask import Flask
+from pymongo import MongoClient
+
+import config
+from lib.redisIO import RedisIO
+
+TESTING = True if os.environ.get("TESTING") else False
+
+DISCORD_API = 'https://discordapp.com/api'
 
 app = Flask(__name__)
+rdb = RedisIO(config.redis_url if not TESTING else config.test_redis_url)
+mdb = MongoClient(config.mongo_url if not TESTING else config.test_mongo_url).avrae
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def hello_world():
     return 'Hello World!'
 
