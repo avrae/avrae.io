ae68f3f4cbbd377abd416c52427b7846f763b7d3
Andrew Zhu <mommothazaz123@gmail.com>
2018-09-03 14:52:48 -0700

    add POST /aliases/:name

--- backend/blueprints/customizations.py
+++ backend/blueprints/customizations.py
@@ -1,7 +1,7 @@
 from bson.json_util import dumps
-from flask import Blueprint
+from flask import Blueprint, request
 
-from app import mdb
+from app import mdb, rdb
 from lib.discord import get_user_info
 
 customizations = Blueprint('customizations', __name__)
@@ -23,6 +23,27 @@ def alias_list():
     user = get_user_info()
     data = list(mdb.aliases.find({"owner": user.id}))
     return dumps(data)
+
+
+@customizations.route("/aliases/<name>", methods=["POST"])
+def alias_update(name):
+    user = get_user_info()
+    data = request.json
+    if data is None:
+        return "No data found", 400
+    if 'commands' not in data:
+        return "Missing commands field", 400
+    if not data['commands']:
+        return "Commands cannot be blank", 400
+    if " " in name:
+        return "Name cannot contain whitespace", 400
+    if name in rdb.jget("default_commands", []):
+        return "Alias is already built-in", 409
+    if len(data['commands']) > 4000:
+        return "Alias commands must be less than 4KB", 400
+    
+    mdb.aliases.update_one({"owner": user.id, "name": name}, {"$set": {"commands": data['commands']}}, upsert=True)
+    return "Alias updated."
 
 
 @customizations.route("/aliases/<name>", methods=["DELETE"])
