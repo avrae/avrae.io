c3f99d86ad65c54510a48e12ee8b8655c16a7f42
Andrew Zhu <mommothazaz123@gmail.com>
2018-09-20 13:12:53 -0700

    don't show packs without share access

--- frontend/src/app/homebrew-sharing/homebrew-sharing.service.ts
+++ frontend/src/app/homebrew-sharing/homebrew-sharing.service.ts
@@ -3,7 +3,7 @@ import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {Observable, of} from "rxjs";
 import {Pack} from "../schemas/homebrew/Items";
-import {defaultOptions} from "../dashboard/APIHelper";
+import {catchError} from "rxjs/operators";
 
 const itemsUrl = `${environment.apiURL}/homebrew/items`;
 
@@ -16,13 +16,6 @@ export class HomebrewSharingService {
   }
 
   getPack(id): Observable<Pack> {
-    return this.http.get<Pack>(`${itemsUrl}/${id}`, defaultOptions());
-  }
-
-  private handleError<T>(operation = 'operation') {
-    return (error: any): Observable<object> => {
-      console.error(error); // log to console and hope for the best
-      return of({error: `${operation} failed: ${error.error}`, success: false});
-    };
+    return this.http.get<Pack>(`${itemsUrl}/${id}`);
   }
 }
