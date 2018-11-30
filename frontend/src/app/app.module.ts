33d003ab8f5b533208b2d7a41b0538d4f7d4d77a
cfi <carlo@field.li>
2018-11-30 18:59:43 +0100

    remove unnecessary import

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,7 +12,6 @@ import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {FooterComponent} from './footer/footer.component';
-import {LoginComponent} from './login/login.component';
 import {ThemesModule} from "./themes/themes.module";
 import {ErrorComponent} from './error/error.component';
 import {FormsModule} from "@angular/forms";
