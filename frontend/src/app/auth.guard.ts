b2369b4bb020afaa9796e2d841b443292d3dd94f
cfi <carlo@field.li>
2018-11-30 19:22:48 +0100

    update auth-guard

---
+++ frontend/src/app/auth.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import {isLoggedIn} from "./SecurityHelper";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return isLoggedIn();
+  }
+}
--- frontend/src/app/dashboard/dashboard-routing.module.ts
+++ frontend/src/app/dashboard/dashboard-routing.module.ts
@@ -4,12 +4,12 @@ import {DashboardComponent} from "./dashboard.component";
 import {CharactersComponent} from "./characters/characters.component";
 import {CustomizationComponent} from "./customization/customization.component";
 import {GvarsComponent} from "./gvars/gvars.component";
-import {AuthGuardService} from "../auth-guard.service";
+import {AuthGuard} from "../auth.guard";
 
 
 const dashboardRoutes: Routes = [
   {
-    path: "dashboard", canActivate: [AuthGuardService], component: DashboardComponent, children: [
+    path: "dashboard", canActivate: [AuthGuard], component: DashboardComponent, children: [
       {path: "characters", component: CharactersComponent},
       // {path: "characters/:id"}
       {path: "aliases", component: CustomizationComponent},
