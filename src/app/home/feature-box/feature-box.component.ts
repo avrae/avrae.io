2911f043e85d42fb78e8e1ff20689f198ea895a1
Andrew Zhu <me@andrew-zhu.com>
2020-05-15 16:17:23 -0700

    as it turns out, I am easily nerd sniped

--- src/app/home/feature-box/feature-box.component.ts
+++ src/app/home/feature-box/feature-box.component.ts
@@ -1,16 +1,20 @@
+import {BreakpointObserver} from '@angular/cdk/layout';
 import {Component, Input, OnInit} from '@angular/core';
+import {BreakpointBaseComponent} from '../../shared/breakpoints';
 
 @Component({
   selector: 'avr-feature-box',
   templateUrl: './feature-box.component.html',
   styleUrls: ['./feature-box.component.scss']
 })
-export class FeatureBoxComponent implements OnInit {
+export class FeatureBoxComponent extends BreakpointBaseComponent implements OnInit {
 
   @Input() title: string;
   @Input() image: string;
+  @Input() image_left = true;
 
-  constructor() {
+  constructor(private bp: BreakpointObserver) {
+    super(bp);
   }
 
   ngOnInit(): void {
