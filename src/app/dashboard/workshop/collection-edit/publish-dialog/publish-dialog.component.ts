849ec5bb63c2bdde1ba451e76d2c91f5239491a9
Andrew Zhu <me@andrew-zhu.com>
2020-09-18 15:01:22 -0700

    update rules doc

--- src/app/dashboard/workshop/collection-edit/publish-dialog/publish-dialog.component.ts
+++ src/app/dashboard/workshop/collection-edit/publish-dialog/publish-dialog.component.ts
@@ -7,7 +7,7 @@ const PUBLISH_STEPS = [
   `I understand that after publishing this collection, I <b>cannot delete or unpublish</b> it or any item contained in it.`,
   `If any item in this collection uses rules or data that is not in the System Reference Document, I have added the required license(s) to that item.`,
   `Aside from any data which I have added the license to use, I and/or this collection's editors am/are the sole owner(s) of the content in this collection.`,
-  `I agree to sections 1 and 2 of the <a href="https://www.dndbeyond.com/homebrew-rules-guidelines" target="_blank">D&D Beyond Homebrew Rules & Guidelines</a>, "Unacceptable content for Public Homebrew" and "Included topics that will result in a ban."`
+  `I agree to the <a href="https://github.com/avrae/.github/blob/master/community/alias-workshop-rules.md" target="_blank">Alias Workshop Rules & Guidelines</a>."`
 ];
 
 @Component({
