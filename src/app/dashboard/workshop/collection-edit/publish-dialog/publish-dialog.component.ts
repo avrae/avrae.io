0286997308d17ad25d82f291af056cbc332a5368
Andrew Zhu <me@andrew-zhu.com>
2020-09-29 14:04:08 -0700

    fix errant quote

--- src/app/dashboard/workshop/collection-edit/publish-dialog/publish-dialog.component.ts
+++ src/app/dashboard/workshop/collection-edit/publish-dialog/publish-dialog.component.ts
@@ -7,7 +7,7 @@ const PUBLISH_STEPS = [
   `I understand that after publishing this collection, I <b>cannot delete or unpublish</b> it or any item contained in it.`,
   `If any item in this collection uses rules or data that is not in the System Reference Document, I have added the required license(s) to that item.`,
   `Aside from any data which I have added the license to use, I and/or this collection's editors am/are the sole owner(s) of the content in this collection.`,
-  `I agree to the <a href="https://github.com/avrae/.github/blob/master/community/alias-workshop-rules.md" target="_blank">Alias Workshop Rules & Guidelines</a>."`
+  `I agree to the <a href="https://github.com/avrae/.github/blob/master/community/alias-workshop-rules.md" target="_blank">Alias Workshop Rules & Guidelines</a>.`
 ];
 
 @Component({
