639d59e877596e85d381b901c4d6021b049bae31
Andrew Zhu <me@andrew-zhu.com>
2020-12-14 13:11:29 -0800

    WEB-076 server-specific rule in publish dialog
    Resolves #394

--- src/app/dashboard/workshop/collection-edit/publish-dialog/publish-dialog.component.ts
+++ src/app/dashboard/workshop/collection-edit/publish-dialog/publish-dialog.component.ts
@@ -7,6 +7,7 @@ const PUBLISH_STEPS = [
   `I understand that after publishing this collection, I <b>cannot delete or unpublish</b> it or any item contained in it.`,
   `If any item in this collection uses rules or data that is not in the System Reference Document, I have added the required license(s) to that item.`,
   `Aside from any data which I have added the license to use, I and/or this collection's editors am/are the sole owner(s) of the content in this collection.`,
+  `The code contained in this collection is for anyone to use, and <b>not intended for one specific Discord server</b>.`,
   `I agree to the <a href="https://github.com/avrae/.github/blob/master/community/alias-workshop-rules.md" target="_blank">Alias Workshop Rules & Guidelines</a>.`
 ];
 
