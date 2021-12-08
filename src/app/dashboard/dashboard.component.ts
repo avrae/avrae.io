b3e74ef357bd5a4069241889b01d92d329e7c019
Joseph Keen <icrona@gmail.com>
2021-12-08 14:32:06 -0700

    Add a toggle button for the sidenav on smaller screens (#612)
    * Update dashboard.component.html
    
    * Update dashboard.component.scss
    
    * Update dashboard.component.ts
    
    * misc: formatting, sort imports
    
    Co-authored-by: Andrew Zhu <me@andrew-zhu.com>

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
+import {BreakpointObserver} from '@angular/cdk/layout';
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 import {isLoggedIn, removeToken} from '../SecurityHelper';
+import {BreakpointBaseComponent} from '../shared/breakpoints';
 import {DashboardService} from './dashboard.service';
 
 @Component({
@@ -8,9 +10,10 @@ import {DashboardService} from './dashboard.service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent extends BreakpointBaseComponent {
 
-  constructor(private router: Router, private dashboardService: DashboardService) {
+  constructor(private router: Router, private dashboardService: DashboardService, private bp: BreakpointObserver) {
+    super(bp);
     if (!isLoggedIn()) {
       this.router.navigate(['login']);
     }
