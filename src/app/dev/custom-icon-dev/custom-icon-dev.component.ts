24b51700222d91039e0fa6c32b1f65238e3efaac
Andrew Zhu <me@andrew-zhu.com>
2022-06-13 17:30:07 -0700

    feat: custom ddb icons, icon dev route

---
+++ src/app/dev/custom-icon-dev/custom-icon-dev.component.ts
@@ -0,0 +1,30 @@
+import {Component} from '@angular/core';
+import {MatIconRegistry} from '@angular/material/icon';
+import {DomSanitizer} from '@angular/platform-browser';
+
+@Component({
+  selector: 'avr-custom-icon-dev',
+  templateUrl: './custom-icon-dev.component.html',
+  styleUrls: ['./custom-icon-dev.component.css']
+})
+export class CustomIconDevComponent {
+  constructor(private iconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    iconRegistry.addSvgIconResolver((name, namespace) => {
+      if (namespace === 'ddb') {
+        return domSanitizer.bypassSecurityTrustResourceUrl(`/assets/icon/${name}.svg`);
+      }
+      return null;
+    });
+  }
+
+  CUSTOM_ICONS: string[] = [
+    'digital-dice',
+    'fire',
+    'force',
+    'healing',
+    'melee-attack',
+    'piercing',
+    'ranged-attack',
+    'spells'
+  ];
+}
