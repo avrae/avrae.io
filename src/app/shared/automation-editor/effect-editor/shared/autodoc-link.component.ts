d66215bd58249c32db9ec754c679f35e4214dfc8
Joseph Keen <icrona@gmail.com>
2022-07-07 17:19:39 -0600

    TS Backend for other changes

--- src/app/shared/automation-editor/effect-editor/shared/autodoc-link.component.ts
+++ src/app/shared/automation-editor/effect-editor/shared/autodoc-link.component.ts
@@ -3,9 +3,9 @@ import {Component, Input, OnInit} from '@angular/core';
 @Component({
   selector: 'avr-autodoc-link',
   template: `
-    <a [href]="href" target="_blank">View the documentation on ReadTheDocs.</a>
+    <a [href]="href" target="_blank" class="doc-link">View the documentation on ReadTheDocs.</a>
   `,
-  styles: []
+  styles: ['.doc-link { display: block; margin-top: 8px; }']
 })
 export class AutodocLinkComponent {
   @Input() anchor: string;
