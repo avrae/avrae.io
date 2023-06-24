caff89371299faaf20f949d1ad4ead39ba30c4c1
KedarOthort <119468349+KedarOthort@users.noreply.github.com>
2023-06-23 22:21:06 -0400

    Update new-effect-button.component.ts
    Making an edit to the premade Grapple automation to allow for bonuses to be passed to the user's grapple check

--- src/app/shared/automation-editor/new-effect-button.component.ts
+++ src/app/shared/automation-editor/new-effect-button.component.ts
@@ -382,86 +382,133 @@ export class NewEffectButtonComponent implements OnInit {
   doGrapplePreset() {
     const effects = [
       {
-        type: "target",
-        target: 1,
-        effects: [
+        "type": "target",
+        "target": "self",
+        "effects": [
           {
-            type: "check",
-            ability: ["acrobatics", "athletics"],
-            contestAbility: ["athletics"],
-            success: [],
-            fail: [
-              {
-                type: "ieffect2",
-                name: "Grappled",
-                desc: "Grappled by {{caster.name}}",
-                buttons: [
-                  {
-                    label: "Escape Grapple",
-                    verb: "attempts to break out of the grapple",
-                    style: "3",
-                    automation: [
-                      {
-                        type: "target",
-                        target: "children",
-                        effects: [
-                          {
-                            type: "check",
-                            ability: ["athletics"],
-                            contestAbility: ["acrobatics", "athletics"],
-                            success: [],
-                            fail: [
-                              {
-                                type: "remove_ieffect"
-                              } as RemoveIEffect
-                            ],
-                            contestTie: "fail"
-                          } as AbilityCheck
-                        ]
-                      } as Target
-                    ]
-                  } as ButtonInteraction
-                ],
-                save_as: "grapple"
-              } as IEffect
-            ],
-            contestTie: "neither"
+            "type": "check",
+            "ability": "athletics"
           } as AbilityCheck
         ]
       } as Target,
       {
-        type: "condition",
-        condition: "not lastCheckDidPass",
-        onTrue: [
+        "type": "variable",
+        "name": "Grapple",
+        "value": "lastCheckRollTotal"
+      } as SetVariable,
+      {
+        "type": "target",
+        "target": 1,
+        "effects": [
           {
-            type: "target",
-            target: "self",
-            effects: [
+            "type": "check",
+            "ability": [
+              "acrobatics",
+              "athletics"
+            ],
+            "dc": "Grapple",
+            "fail": [
               {
-                type: "ieffect2",
-                name: "Grappling",
-                desc: "Grappling {{targets[0].name if str(targets[0])!=targets[0] else targets[0]}}",
-                buttons: [
+                "type": "ieffect2",
+                "name": "Grappling {{targets[0].name if str(targets[0])!=targets[0] else targets[0]}}",
+                "duration": null,
+                "effects": null,
+                "attacks": [],
+                "buttons": [
                   {
-                    label: "Release Grapple",
-                    verb: "lets go of their target",
-                    style: "1",
-                    automation: [
+                    "label": "Release {{targets[0].name if str(targets[0])!=targets[0] else targets[0]}}",
+                    "automation": [
                       {
-                        type: "remove_ieffect",
-                        removeParent: "always"
+                        "type": "remove_ieffect",
+                        "removeParent": "always"
                       } as RemoveIEffect
-                    ]
+                    ],
+                    "verb": "lets go of {{targets[0].name if str(targets[0])!=targets[0] else targets[0]}}",
+                    "style": "3"
                   } as ButtonInteraction
                 ],
-                parent: "grapple"
+                "end": false,
+                "conc": false,
+                "desc": null,
+                "stacking": false,
+                "save_as": "grappling",
+                "parent": null,
+                "target_self": true
+              } as IEffect,
+              {
+                "type": "ieffect2",
+                "name": "Grappled",
+                "duration": null,
+                "effects": null,
+                "attacks": [],
+                "buttons": [
+                  {
+                    "label": "Escape Grapple",
+                    "automation": [
+                      {
+                        "type": "target",
+                        "target": "parent",
+                        "effects": [
+                          {
+                            "type": "check",
+                            "ability": [
+                              "athletics"
+                            ]
+                          } as AbilityCheck,
+                          {
+                            "type": "variable",
+                            "name": "maintainGrapple",
+                            "value": "lastCheckRollTotal"
+                          } as SetVariable
+                        ]
+                      } as Target,
+                      {
+                        "type": "target",
+                        "target": "self",
+                        "effects": [
+                          {
+                            "type": "check",
+                            "ability": [
+                              "athletics",
+                              "acrobatics"
+                            ],
+                            "dc": "maintainGrapple",
+                            "success": [
+                              {
+                                "type": "remove_ieffect",
+                                "removeParent": "if_no_children"
+                              } as RemoveIEffect
+                            ],
+                            "fail": []
+                          } as AbilityCheck
+                        ]
+                      } as Target
+                    ],
+                    "verb": "attempts to break out of the grapple",
+                    "style": "4",
+                    "defaultDC": null,
+                    "defaultAttackBonus": null,
+                    "defaultCastingMod": null
+                  } as ButtonInteraction
+                ],
+                "end": false,
+                "conc": false,
+                "desc": "Grappled by {{caster.name}}",
+                "stacking": false,
+                "save_as": "grapple",
+                "parent": "grappling",
+                "target_self": false
               } as IEffect
-            ]
-          } as Target
-        ],
-        onFalse: [],
-        errorBehaviour: "false"
-      } as Condition
+            ],
+            "success": []
+          } as AbilityCheck
+        ]
+      } as Target,
+      {
+        "type": "text",
+        "text": "When you want to grab a creature or wrestle with it, you can use the Attack action to make a special melee attack, a grapple. If you’re able to make multiple attacks with the Attack action, this attack replaces one of them.\n\nThe target of your grapple must be no more than one size larger than you and must be within your reach. Using at least one free hand, you try to seize the target by making a grapple check instead of an attack roll: a Strength (Athletics) check contested by the target’s Strength (Athletics) or Dexterity (Acrobatics) check (the target chooses the ability to use). You succeed automatically if the target is incapacitated. If you succeed, you subject the target to the grappled condition. The condition specifies the things that end it, and you can release the target whenever you like (no action required).",
+        "title": "Effect"
+      } as Text
     ];
     this.context.parentArray.push(...effects);
     this.created.emit();
@@ -549,6 +596,5 @@ export class NewEffectButtonComponent implements OnInit {
     this.created.emit();
   }
 }
-
 
 
