765d54427d70e86c37cb8fbdf771368797319537
tiberius0954 <Daniel.agudelo@c.wizards.com>
2025-06-03 16:30:14 -0500

    refactor: remove sentry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import * as Sentry from '@sentry/browser';
 import {MarkdownModule, MarkedOptions} from 'ngx-markdown';
 import {MonacoEditorModule} from 'ngx-monaco-editor';
 
@@ -31,8 +30,6 @@ import {DiscordEmbedModule} from './shared/discord-embed/discord-embed.module';
 import {registerDraconicLanguage} from './shared/monacoDraconic';
 import {ValidationSnackbar} from './shared/validation-snackbar/validation-snackbar.component';
 import {ThemesModule} from './themes/themes.module';
-
-Sentry.init({dsn: 'https://af2b06560981446bb55f64b6f79fd520@sentry.io/1486249'});
 
 const markdownConfig = {
   markedOptions: {
