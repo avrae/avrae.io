e119fb12c430e5d8eaf0bc77eb7c52c35ed15785
tiberius0954 <Daniel.agudelo@c.wizards.com>
2025-10-31 16:53:06 -0500

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,10 +73,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run start',
-    url: 'http://127.0.0.1:3000',
-    reuseExistingServer: !process.env.CI,
-    timeout: 240 * 1000
-  },
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  //   timeout: 240 * 1000
+  // },
 });
