0c794ffda80756dfd6a3b43975c725590d53a1ea
tiberius0954 <165932488+tiberius0954@users.noreply.github.com>
2025-11-04 18:41:54 -0500

    fix: pipeline e2e tests preventing deploy (#723)

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,9 +73,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://127.0.0.1:4200',
+    reuseExistingServer: !process.env.CI,
+    timeout: 240 * 1000
+  },
 });
